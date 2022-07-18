5c70cc9762b5ec3b7da389a2399f918ed8e59c29
mariesophiecd <mariesophie.cduran@gmail.com>
2022-07-18 09:27:13 +0100

    added components, pages, actions, reducers folders + installed redux and coverage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/host' element={<Host/>} />
+          <Route path='/client' element={<Client />} />
+          <Route path='/quiz' element={<Quiz />} />
+        </Routes>
       </header>
     </div>
   );
