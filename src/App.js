06c649c0d995fe482f3938f8697827b79b2fcc38
mariesophiecd <mariesophie.cduran@gmail.com>
2022-07-18 14:39:59 +0100

    added favicon + fonts, installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from 'react-router-dom';
 import { Home, Host, Client, Quiz } from './pages';
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src='/images/gear_icon.png' />
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/host' element={<Host/>} />
           <Route path='/client' element={<Client />} />
-          <Route path='/quiz'>
-            <Route path='/theme' element={<Quiz />} />
+          <Route path='/quiz' element={<Quiz />}>
+            {/* <Route path='/theme' element={<Quiz />} />
             <Route path='/question' element='' />
-            <Route path='/final_scores' element=''/>
+            <Route path='/final_scores' element=''/> */}
           </Route>
         </Routes>
       </header>
