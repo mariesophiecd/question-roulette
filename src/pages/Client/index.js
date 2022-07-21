9f9b06448dd604475555b35c5376aa1760f511a0
Ayoub_L <ayoublaissaoui.1@gmail.com>
2022-07-21 08:25:14 +0100

    adjusted the redirectory og the best page

--- src/pages/Client/index.js
+++ src/pages/Client/index.js
@@ -14,15 +14,19 @@ export default function Client() {
   const [button, setButton] = useState(true);
   const [users, setUsers] = useState(0);
   const [roomsAvailable, setRoomsAvailability] = useState([]);
+  const [players, setPlayers] = useState([]);
+  const [username, setUsername] = useState('');
+  const [difficulty, setDifficulty] = useState('');
+  const [searchSubject, setSubject] = useState('');
 
   //to just emit the same event to all members of a room
   // io.to('Room Name').emit('new event', 'Updates');
 
   const joinRoom = () => {
-    // if(!roomsAvailable.includes(room))
-    // {
-    //   return;
-    // }
+    if(!roomsAvailable.includes(room))
+    {
+      return;
+    }
     try{
       if(room !== "") {
         socket.emit("join_room", room);
@@ -33,19 +37,33 @@ export default function Client() {
     }   
   }
 
-  const getUsers = () => { 
-    socket.emit('userNum', () => {
+  const Difficulty = () => {
+    socket.emit("get_difficulty", (data) => {
+        setDifficulty(data);
     })
-  };
+}
+
+  const Subject = () => {
+    socket.emit("get_subject", (data) => {
+        setSubject(data);
+    })
+}
+
+  const changeUsername = () => {
+    socket.emit("set_username", username);
+  }
 
   socket.on('users', data => {
-    console.log('Users are :' + data);
-    setUsers(data);
+      setUsers(data);
   });
 
   const sendMessage = () => {
-       socket.emit("send_message", { message, room });
+      socket.emit("send_message", { message, room });
   };
+
+  socket.on("players", (data) =>{
+    setPlayers(data);
+  })
 
   useEffect(() => {
 
@@ -55,6 +73,7 @@ export default function Client() {
     socket.on("receive_message", (data) => {
       setMessageReceived(data.message);
     })
+    
 
   }, [socket]);
 
@@ -73,15 +92,26 @@ export default function Client() {
           <input placeholder="JOIN ROOM" onChange={(event) => {
             setRoom(event.target.value);
           }}/>
-          <button onClick={joinRoom}> Create Room </button>
+          <button onClick={joinRoom}> Join Room </button>
         </div> : <div><h3> JOINED ROOM: {room}</h3></div>}
 
-        <div> <h3>Users </h3> {users} </div>
+        <div>
+          <h3>Users </h3> {users}
+          <h3>ALLPLAYERS </h3> {players.length}
+          <br></br>
+          <input placeholder="USERNAME" onChange={(event) => {
+            setUsername(event.target.value);
+          }}/>
+          <button onClick={changeUsername}> change username</button>
+        </div>
         <div> <h3> Available Rooms : </h3> {renderAllRooms()} </div>
-      
+
         <input placeholder="Message..." onChange={(event) => {
           setMessage(event.target.value);
         }}/>
+
+        {/* <div> <h3>ALLPLAYERS </h3> {players} </div> */}
+
         <button onClick={sendMessage}> Send Message </button>
         <h1> Message:</h1>
         {messageReceived}
